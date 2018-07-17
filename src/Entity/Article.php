5e9c900f6f48dc862c8cc1131c5d64210184588d
NicolasBadey <nicolas.badey@gmail.com>
2018-07-18 00:53:47 +0200

    add Article entity

---
+++ src/Entity/Article.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setUpdatedAt($id)
+    {
+        $this->id = $id;
+    }
+}
+
+
